c22a325248cf074c2926bb07f0a467e9131adce3
bertinguer <albertoil1982@gmail.com>
2023-01-28 11:39:13 +0100

    Update pdf2imei.py

--- pdf2imei.py
+++ pdf2imei.py
@@ -1,9 +1,9 @@
-mport re
+import re
 import sys
 import PyPDF2
 
 def buscar_imeis(archivo):
-    # Crear una lista vacía para almacenar los IMEI encontrados
+    # Crear un set vacío para almacenar los IMEI encontrados
     imeis_encontrados = set()
 
     # Abrimos el archivo PDF
@@ -20,15 +20,34 @@ def buscar_imeis(archivo):
                 # Buscamos el IMEI en el texto de la página
                 if re.search(r'(?:IMEI|imei)',linea, re.IGNORECASE):
                     imei = lineas[j+1]
-                    if re.search(r'^\d{14,15}$',imei):
-                        imei = imei[:14] if len(imei)==15 else imei
+                    if re.search(r'^\d{14}|\d{15}|\d{16}$',imei):
                         if imei not in imeis_encontrados:
                             imeis_encontrados.add(imei)
+
+    # Imprimimos los IMEIs encontrados
+    for imei in imeis_encontrados:
+        print(f'{imei}')
+
+"""
+        for i in range(len(lector.pages)):
+            pagina = lector.pages[i]
+            texto = pagina.extract_text()
+            lineas = texto.split('\n')
+            imei = ''
+            for j, linea in enumerate(lineas):
+                # Buscamos el IMEI en el texto de la página
+                if re.search(r'(?:IMEI|imei)',linea, re.IGNORECASE):
+                    imei = lineas[j+1]
+                    if re.search(r'^\d{14}|\d{15}|\d{16}$',imei):
+                        #imei = imei[:14] if len(imei)>=14 else imei
+                        #if imei not in imeis_encontrados:
+                            #imeis_encontrados.add(imei)
                             print(f'{imei}')
+"""
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Uso: python pdf2imei.py archivo.pdf')
+        print('Uso: python script.py archivo.pdf')
         sys.exit(1)
 
     archivo = sys.argv[1]
